115805ba6b29910961da2b3604ebcef628999a9f
Yannick Heinrich <yannick.heinrich@gmail.com>
2015-09-27 08:49:25 +0200

    Initial Commit

---
+++ SwiftOAuth1a/SwiftOAuth1a.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOAuth1a.h
+//  SwiftOAuth1a
+//
+//  Created by Yannick Heinrich on 27/09/15.
+//  Copyright © 2015 yageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftOAuth1a.
+FOUNDATION_EXPORT double SwiftOAuth1aVersionNumber;
+
+//! Project version string for SwiftOAuth1a.
+FOUNDATION_EXPORT const unsigned char SwiftOAuth1aVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOAuth1a/PublicHeader.h>
+
+
---
+++ SwiftOAuth1aTests/SwiftOAuth1aTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftOAuth1aTests.swift
+//  SwiftOAuth1aTests
+//
+//  Created by Yannick Heinrich on 27/09/15.
+//  Copyright © 2015 yageek. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftOAuth1a
+
+class SwiftOAuth1aTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
