ef5fcaf7c2e2bbd6cd93b602a0323165255bf400
Yannick Heinrich <yannick.heinrich@gmail.com>
2015-10-13 17:32:58 +0200

    Add OS X target

---
+++ SwitfOAuth1aOSX/SwitfOAuth1aOSX.h
@@ -0,0 +1,19 @@
+//
+//  SwitfOAuth1aOSX.h
+//  SwitfOAuth1aOSX
+//
+//  Created by Yannick Heinrich on 13/10/15.
+//  Copyright © 2015 yageek. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwitfOAuth1aOSX.
+FOUNDATION_EXPORT double SwitfOAuth1aOSXVersionNumber;
+
+//! Project version string for SwitfOAuth1aOSX.
+FOUNDATION_EXPORT const unsigned char SwitfOAuth1aOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwitfOAuth1aOSX/PublicHeader.h>
+
+
